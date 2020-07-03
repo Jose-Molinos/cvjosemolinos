8c1c5d1c54af0a684be18cd281bc3021dad90d44
Jose Molinos <molinossobrino.jose@gmail.com>
2020-07-03 20:43:39 +0200

    HastaOcultarBotones

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+$(document).ready(()=>{
+
+    
+    $("#btnDatos").click(()=>{
+       $("#Datos").show();
+       $("#Experiencia").hide();
+       $("#Formacion").hide();
+       $("#Cursos").hide();
+       $("#Informatica").hide();
+       $("#OtrosDatos").hide();
+       $("#SituacionLaboral").hide();
+
+
+
+    })
+    })
